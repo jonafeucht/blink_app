3a745fae381c16aed9b4bb824736fd09c801fc50
JONA T. FEUCHT <jona@keemail.me>
2022-07-14 15:56:12 +0200

    init routing

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:blinkapp/services/auth/login.dart';
+import 'package:blinkapp/core/routes/routes.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -7,15 +7,15 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MaterialApp.router(
+      routeInformationProvider: Routes.router.routeInformationProvider,
+      routeInformationParser: Routes.router.routeInformationParser,
+      routerDelegate: Routes.router.routerDelegate,
       debugShowCheckedModeBanner: false,
       title: 'Blink',
       theme: ThemeData(),
-      home: const LoginScreen(),
     );
   }
 }
